efdb6da1d6e43b2f781793630830085a148a69f0
mariaa7 <mariaa7@uw.edu>
2014-10-21 09:33:35 -0700

    Made changes to Index HTML and Script JS

--- js/script.js
+++ js/script.js
@@ -22,12 +22,12 @@ for (var prop in person) {
 	console.log(person[prop]);
 }
 
-var elem = document.createElement('img');
-elem.src = 'img/dog.jpg';
-elem.alt = 'picture of my awesome, amazing dog';
+var pup = document.createElement('img');
+pup.src = 'img/dog.jpg';
+pup.alt = 'picture of my awesome, amazing dog';
 
 //add this element to the top of the body
-document.body.insertBefore(elem, document.body.firstChild);
+document.body.insertBefore(pup, document.body.firstChild);
 
 var kitty = document.createElement('img');
 kitty.src = 'img/cat.jpg';
@@ -35,3 +35,22 @@ kitty.alt = 'picture of a cool cat';
 
 //add this to the body of the page
 document.body.appendChild(kitty);
+
+function onPuppyClick() {
+	//alert('you clicked my puppy!');
+	document.getElementById('bark').play();
+}
+
+//selecting the puppy-pic element
+var elem = document.getElementById('puppy-pic');
+
+if (elem.addEventListener) {
+	elem.addEventListener('click', onPuppyClick);
+}
+else if (elem.attachEvent) {
+	elem.attachEvent('click', onPuppyClick);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	alert("The DOM is ready");
+});
